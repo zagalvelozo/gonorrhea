3f09ac5d4c45cc96b27357a940db15e9d8ecf681
jzagal <zagalvelozo@gmail.com>
2023-11-02 12:29:55 -0300

    feat: add healthcheck endpoint

--- server/server.go
+++ server/server.go
@@ -11,13 +11,19 @@ const (
 	VERSION = "0.25.5"
 )
 
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "ok")
+}
+
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, VERSION)
 }
 
 func Serve() {
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", getRoot)
+	mux.healthCheck("/health", healthCheck)
 
 	err := http.ListenAndServe(":3333", mux)
 	if err != nil {
